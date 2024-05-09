4ce23b293046447d17c49bc0777b0605a9b791e3
Michael Sylvester <42616131+Michael-Sylvester@users.noreply.github.com>
2024-05-09 16:00:41 +0000

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,35 @@
-#!/usr/bin/env bash
-#Install Nginx is not installed and prepare file structure
+#!/usr/bin/env  bash
+
 # Check if Nginx is installed
 if ! [ -x "$(command -v nginx)" ]; then
   echo "Installing Nginx..."
-  apt-get update
-  apt-get install -y nginx
+  sudo apt-get update
+  sudo apt-get install -y nginx
 fi
+echo "Nginx installation status: $?"
 
 # Create necessary directories if they don't exist
-mkdir -p /data/web_static/{releases/test,shared}
+sudo mkdir -p /data/web_static/{releases/test,shared}
+echo "Directory creation status: $?"
 
 # Create a fake HTML file for testing
-echo "<html><head><title>Test Page</title></head><body><h1>This is a test page</h1></body></html>" | tee /data/web_static/releases/test/index.html > /dev/null
+sudo echo "<html><head><title>Test Page</title></head><body><h1>This is a test page</h1></body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+echo "HTML createion status: $?"
 
-# Create symbolic link
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Create symbolic link for test directory named current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+echo "Symbolic link status: $?"
 
-# Give ownership of /data folder to ubuntu user and group recursively
-chown -R ubuntu:ubuntu /data
+# Give ubuntu user and group ownership of /data
+sudo chown -R ubuntu:ubuntu /data
+echo "FIle ownership status: $?"
 
-# Update Nginx configuration
+# Update Nginx configuration if necessary
 nginx_config="/etc/nginx/sites-available/default"
-sed -i '/^server {/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' $nginx_config
-
-# Restart Nginx
-systemctl restart nginx
+if ! grep -q "location /hbnb_static/" $nginx_config; then
+  echo "Updating Nginx configuration..."
+  sudo sed -i '/^server {/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' $nginx_config
+  sudo service nginx restart
+else
+  echo "Nginx configuration already contains the necessary settings."
+fi
