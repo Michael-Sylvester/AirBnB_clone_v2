d0b66d35d8a5b565d51c6b2260b4c536db53b728
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-05-13 14:56:01 +0100

    task 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Fabric script to deploy an archive to web servers.
+"""
+from fabric.api import env, put, run
+import os
+
+# Define the list of web servers
+env.hosts = ['54.160.115.226', '100.25.139.174']
+
+
+def do_deploy(archive_path):
+    """
+    Distributes an archive to web servers.
+    Args:
+        archive_path: Path to the archive to be deployed.
+    Returns:
+        True if all operations have been done correctly, otherwise False.
+    """
+    if not os.path.exists(archive_path):
+        print("Archive not found.")
+        return False
+
+    try:
+        # Upload the archive to /tmp/ directory on the web servers
+        put(archive_path, '/tmp/')
+
+        archive_filename = os.path.basename(archive_path)
+        release_folder = '/data/web_static/releases/{}'.format(
+            archive_filename[:-4])
+        run('mkdir -p {}'.format(release_folder))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_folder))
+
+        # Delete the archive from the web servers
+        run('rm /tmp/{}'.format(archive_filename))
+
+        # Delete the symbolic link /data/web_static/current
+        current_link = '/data/web_static/current'
+        run('rm -f {}'.format(current_link))
+
+        # Create a new symbolic link for the new version
+        run('ln -s {} {}'.format(release_folder, current_link))
+
+        print("New version deployed successfully.")
+        return True
+    except Exception as e:
+        print("An error occurred:", e)
+        return False
