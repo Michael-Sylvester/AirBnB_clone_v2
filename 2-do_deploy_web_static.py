fe368b61b8f446cd5d1fa95bfc6abcf598b265f2
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-05-13 18:55:37 +0100

    task 2: update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,8 @@
 """
 Fabric script to deploy an archive to web servers.
 """
-from fabric.api import env, put, run
+from fabric.api import env, put, run, local
+from datetime import datetime
 import os
 
 # Define the list of web servers
@@ -38,14 +39,14 @@ def do_deploy(archive_path):
         run('sudo rm /tmp/{}'.format(archive_filename))
 
         # Move contents of web_static folder to release folder
-        # run('sudo mv  {}/web_static/* {}'.format(
-        # release_folder, release_folder))
-        #   Remove web_static folder from release folder
+        run('sudo mv  {}/web_static/* {}'.format(
+            release_folder, release_folder))
+        # Remove web_static folder from release folder
         run('sudo rm -rf {}/web_static'.format(release_folder))
 
         # Delete the symbolic link /data/web_static/current
         current_link = '/data/web_static/current'
-        run('if [ -L {} ]; then sudo rm {}; fi'.format(
+        run('if [ -L {} ]; then sudo rm -rf {}; fi'.format(
             current_link, current_link))
 
         # Create a new symbolic link for the new version
@@ -56,3 +57,25 @@ def do_deploy(archive_path):
     except Exception as e:
         print("An error occurred:", e)
         return False
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static folder.
+    Returns:
+        Path to the generated archive if successful, None otherwise.
+    """
+    try:
+        # Create the versions folder if it doesn't exist
+        local("mkdir -p versions")
+
+        # Generate timestamp for archive name
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+
+        # Create the .tgz archive with the timestamp in its name
+        archive_name = "versions/web_static_{}.tgz".format(timestamp)
+        local("tar -cvzf {} web_static".format(archive_name))
+
+        return archive_name
+    except Exception as e:
+        print("An error occurred:", e)
+        return None
